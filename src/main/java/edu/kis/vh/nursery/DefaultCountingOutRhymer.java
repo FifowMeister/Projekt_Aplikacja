8a1d4c29bb714354cf327146bf74234c46a1febd
Filip Barszczyk <245764@edu.p.lodz.pl>
2024-05-26 21:54:49 +0200

    12. Usuniecie nieuzywanych getterow i setterow

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -34,14 +34,6 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
 }
 
 
