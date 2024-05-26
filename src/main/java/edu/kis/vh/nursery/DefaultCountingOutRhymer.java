d4163f22170b87b4a81f4a6fd030ab37c89da69b
Filip Barszczyk <245764@edu.p.lodz.pl>
2024-05-26 21:29:42 +0200

    10. Wygenerowanie getter oraz setter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -34,4 +34,14 @@ public class DefaultCountingOutRhymer {
         return numbers[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
 }
+
+
